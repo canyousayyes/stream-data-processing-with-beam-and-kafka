8a29c091e9705475f3edc1663baa41236bb9fee0
Gene Ng <fishballium@gmail.com>
2019-01-28 18:21:43 +0800

    Update to use Scala 2.11 instead

--- wordcount-spark-runner-example/wordcount-app/src/main/scala/com/example/beam/App.scala
+++ wordcount-spark-runner-example/wordcount-app/src/main/scala/com/example/beam/App.scala
@@ -1,11 +1,12 @@
 package com.example.beam
 
-import collection.JavaConverters._
+import java.lang.{Long => JLong}
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.options.Validation.Required
 import org.apache.beam.sdk.options.{Default, Description, PipelineOptions, PipelineOptionsFactory}
-import org.apache.beam.sdk.transforms.{Count, FlatMapElements, MapElements}
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.transforms._
 import org.apache.beam.sdk.values.{KV, TypeDescriptors}
 import org.slf4j.LoggerFactory
 
@@ -29,6 +30,23 @@ object App {
     def setAwsRegion(value: String): Unit
   }
 
+  def extractWords = new DoFn[String, String] {
+    @ProcessElement
+    def processElement(c: ProcessContext): Unit = {
+      for (word <- c.element().split("[^\\p{L}]+")) yield {
+        if (!word.isEmpty) {
+          c.output(word)
+        }
+      }
+    }
+  }
+
+  def formatResults = new SimpleFunction[KV[String, JLong], String] {
+    override def apply(input: KV[String, JLong]): String = {
+      input.getKey + ": " + input.getValue
+    }
+  }
+
   def main(args: Array[String]) {
     val options = PipelineOptionsFactory
       .fromArgs(args: _*)
@@ -42,12 +60,10 @@ object App {
 
     LOG.info(s"Running wordcount example from ${options.getInputPath} to ${options.getOutputPath} ...")
     pipeline.apply("ReadFiles", input)
-      .apply("ExtractWords", FlatMapElements.into(TypeDescriptors.strings)
-        .via((word: String) => word.split("[^\\p{L}]+").toIterable.asJava)
-      )
+      .apply("ExtractWords", ParDo.of(extractWords))
       .apply("CountWords", Count.perElement[String])
       .apply("FormatResults", MapElements.into(TypeDescriptors.strings)
-        .via((input: KV[String, java.lang.Long]) => s"${input.getKey}: ${input.getValue}")
+        .via(formatResults)
       )
       .apply("WriteFiles", output)
 
--- wordcount-spark-runner-example/wordcount-app/src/main/scala/com/example/beam/MinimalApp.scala
+++ wordcount-spark-runner-example/wordcount-app/src/main/scala/com/example/beam/MinimalApp.scala
@@ -1,13 +1,31 @@
 package com.example.beam
 
-import collection.JavaConverters._
+import java.lang.{Long => JLong}
 import org.apache.beam.sdk.Pipeline
 import org.apache.beam.sdk.io.TextIO
 import org.apache.beam.sdk.options.PipelineOptionsFactory
-import org.apache.beam.sdk.transforms.{Count, FlatMapElements, MapElements}
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.transforms._
 import org.apache.beam.sdk.values.{KV, TypeDescriptors}
 
 object MinimalApp {
+  def extractWords = new DoFn[String, String] {
+    @ProcessElement
+    def processElement(c: ProcessContext): Unit = {
+      for (word <- c.element().split("[^\\p{L}]+")) yield {
+        if (!word.isEmpty) {
+          c.output(word)
+        }
+      }
+    }
+  }
+
+  def formatResults = new SimpleFunction[KV[String, JLong], String] {
+    override def apply(input: KV[String, JLong]): String = {
+      input.getKey + ": " + input.getValue
+    }
+  }
+
   def main(args: Array[String]) {
     val options = PipelineOptionsFactory.create
     val pipeline = Pipeline.create(options)
@@ -16,12 +34,10 @@ object MinimalApp {
     val output = TextIO.write.to("output/wordcount")
 
     pipeline.apply("ReadFiles", input)
-      .apply("ExtractWords", FlatMapElements.into(TypeDescriptors.strings)
-        .via((word: String) => word.split("[^\\p{L}]+").toIterable.asJava)
-      )
+      .apply("ExtractWords", ParDo.of(extractWords))
       .apply("CountWords", Count.perElement[String])
       .apply("FormatResults", MapElements.into(TypeDescriptors.strings)
-        .via((input: KV[String, java.lang.Long]) => s"${input.getKey}: ${input.getValue}")
+        .via(formatResults)
       )
       .apply("WriteFiles", output)
 
